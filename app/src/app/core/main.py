b8ea2960a5bf115fd85bb39cd070c354d55eeab8
Elias Chokeir <ltpie12345+github.com>
2025-02-26 19:36:06 -0800

    added first gwar document

--- app/src/app/core/main.py
+++ app/src/app/core/main.py
@@ -13,3 +13,13 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.post("/register")
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(80), unique=True, index=True)
+    email = Column(String(120), unique=True, index=True)
+    password_hash = Column(String(128))
